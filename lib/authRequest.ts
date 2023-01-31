5b4e2a1995bc19cb7c0bbaacc47e54c4b6e89908
Mike Drenth <mdrenth.1990@gmail.com>
2023-01-31 20:11:05 +0100

    Nieuwe changes
    Kleine aanpassingen

--- lib/authRequest.ts
+++ lib/authRequest.ts
@@ -1,25 +1,25 @@
-const TOKEN_ENDPOINT = process.env.AUTH_URL as string
+const TOKEN_ENDPOINT = process.env.AUTH_URL as string;
 
 // Request doen om een bearer token aan te maken voor de api requests
 export const getAccessToken = async (site: string) => {
-  if (!site) throw new Error('Geen geldige website opgegeven.')
+  if (!site) throw new Error("Geen geldige website opgegeven.");
 
-  // Dynamisch de env variable ophalen adhv meegeven site
-  const AUTH_USER = `${site}_AUTH_USERNAME`
-  const AUTH_PASSWORD = `${site}_AUTH_PASSWORD`
+  // De juiste user info ophalen in lokaal env bestand
+  const AUTH_USER = `${site}_AUTH_USERNAME`;
+  const AUTH_PASSWORD = `${site}_AUTH_PASSWORD`;
   const body = {
     user: process.env[AUTH_USER],
     password: process.env[AUTH_PASSWORD],
-  }
+  };
 
   const response = await fetch(TOKEN_ENDPOINT, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      'Cache-Control': 'private max-age=86400 immutable',
+      "Content-Type": "application/json",
+      "Cache-Control": "public max-age=86400 immutable",
     },
     body: JSON.stringify(body),
-  })
+  });
 
-  return await response.json()
-}
+  return await response.json();
+};
--- lib/getWebsiteInfo.ts
+++ lib/getWebsiteInfo.ts
@@ -1,45 +1,45 @@
-import { getAccessToken } from './authRequest'
-const API_URL = process.env.API_URL as string
+import { getAccessToken } from "./authRequest";
+const API_URL = process.env.API_URL as string;
 
-// Fetch naar alle websites doen
-const fetchAllSites = async (site: string) => {
-  const { token } = await getAccessToken(site)
-  if (!token) throw new Error('Geen geldige token opgegeven.')
-  try {
-    return fetch(`${API_URL}sites`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'Cache-Control': 'private max-age=900 immutable',
-      },
-    })
-  } catch (error) {
-    console.log(error)
-  }
-}
+// // Fetch naar alle websites doen
+// const fetchAllSites = async (site: string) => {
+//   const { token } = await getAccessToken(site)
+//   if (!token) throw new Error('Geen geldige token opgegeven.')
+//   try {
+//     return fetch(`${API_URL}sites`, {
+//       headers: {
+//         Authorization: `Bearer ${token}`,
+//         'Cache-Control': 'private max-age=900 immutable',
+//       },
+//     })
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
 
 // Aan de hand van domain een website ophalen
 export const fetchSite = async (site: string) => {
-  const { token } = await getAccessToken(site)
-  if (!token) throw new Error('Geen geldige token opgegeven.')
+  const { token } = await getAccessToken(site);
+  if (!token) throw new Error("Geen geldige token opgegeven.");
 
-  const SITE = `${site}_DOMAIN`
-  const DOMAIN = process.env[SITE]
+  const SITE = `${site}_DOMAIN`;
+  const DOMAIN = process.env[SITE];
   try {
     return fetch(`${API_URL}sites?filter[domains.domain_name]=${DOMAIN}`, {
       headers: {
         Authorization: `Bearer ${token}`,
-        'Cache-Control': 'private max-age=900 immutable',
+        "Cache-Control": "private max-age=900 immutable",
       },
-    })
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 // Alle pagina's ophalen aan de hand van site ID
 export const fetchPages = async (site: string, siteId: number) => {
-  const { token } = await getAccessToken(site)
-  if (!token) throw new Error('Geen geldige token opgegeven.')
+  const { token } = await getAccessToken(site);
+  if (!token) throw new Error("Geen geldige token opgegeven.");
 
   try {
     return fetch(
@@ -47,39 +47,66 @@ export const fetchPages = async (site: string, siteId: number) => {
       {
         headers: {
           Authorization: `Bearer ${token}`,
-          'Cache-Control': 'private max-age=900 immutable',
+          "Cache-Control": "private max-age=900 immutable",
         },
       }
-    )
+    );
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
+
+// const fetchLayouts = async (site: string, siteId: number, pageId: number) => {
+//   try {
+//     const { token } = await getAccessToken(site);
+//     const response = await fetch(
+//       `${API_URL}sites/${siteId}/pages/${pageId}?include=layoutRows.columns.col`,
+//       {
+//         headers: {
+//           Authorization: `Bearer ${token}`,
+//           "Cache-Control": "private max-age=900 immutable",
+//         },
+//       }
+//     );
+
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! status: ${response.status}`);
+//     }
+
+//     return await response.json();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
 const fetchLayouts = async (site: string, siteId: number, pageId: number) => {
-  try {
-    const { token } = await getAccessToken(site)
-    const headers = new Headers()
-    headers.append('Content-Type', 'application/json')
-    headers.append('Authorization', `Bearer ${token}`)
-
-    const response = await fetch(
-      `${API_URL}sites/${siteId}/pages/${pageId}?include=layoutRows.columns.col`,
-      {
-        method: 'GET',
-        headers: headers,
-      }
-    )
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`)
+  const { token } = await getAccessToken(site);
+  const response = await fetch(
+    `${API_URL}sites/${siteId}/pages/${pageId}?include=layoutRows.columns.col`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Cache-Control": "private max-age=900 immutable",
+      },
     }
+  );
 
-    return await response.json()
-  } catch (error) {
-    console.log(error)
+  const { data, errors } = await response.json();
+
+  if (response.ok) {
+    // Layouts terug sturen
+
+    return;
+  } else {
+    // error terug geven
   }
-}
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  return await response.json();
+};
 
 // Pagina info opzoeken aan de hand van pagina url
 export const fetchPageInfo = async (
@@ -87,38 +114,39 @@ export const fetchPageInfo = async (
   siteId: number,
   pageUrl: string
 ) => {
-  const { token } = await getAccessToken(site)
-  if (!token) throw new Error('Geen geldige token opgegeven')
+  const { token } = await getAccessToken(site);
+  if (!token) throw new Error("Geen geldige token opgegeven");
   try {
     return fetch(
       `${API_URL}sites/${siteId}/pages?filter[page_url]=${pageUrl}`,
       {
         headers: {
           Authorization: `Bearer ${token}`,
-          'Cache-Control': 'private max-age=900 immutable',
+          "Cache-Control": "private max-age=900 immutable",
         },
       }
-    )
+    );
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 export const getPages = async (site: string, siteId: number): Promise<void> => {
-  if (!site || !siteId) throw new Error('Geen geldige site of siteId opgegeven')
-  const response = await fetchPages(site, siteId)
-  const { data } = await response?.json()
+  if (!site || !siteId)
+    throw new Error("Geen geldige site of siteId opgegeven");
+  const response = await fetchPages(site, siteId);
+  const { data } = await response?.json();
 
-  return data
-}
+  return data;
+};
 
 export const getSiteInfo = async (site: string): Promise<void> => {
-  if (!site) throw new Error('Geen geldige site opgegeven')
-  const response = await fetchSite(site)
-  const { data } = await response?.json()
+  if (!site) throw new Error("Geen geldige site opgegeven");
+  const response = await fetchSite(site);
+  const { data } = await response?.json();
 
-  return data
-}
+  return data;
+};
 
 export const getPageInfo = async (
   site: string,
@@ -126,12 +154,12 @@ export const getPageInfo = async (
   pageUrl: string
 ) => {
   if (!site || !siteId || !pageUrl)
-    throw new Error('Geen geldige site, siteId of pageId opgegeven')
-  const response = await fetchPageInfo(site, siteId, pageUrl)
-  const { data } = await response?.json()
+    throw new Error("Geen geldige site, siteId of pageId opgegeven");
+  const response = await fetchPageInfo(site, siteId, pageUrl);
+  const { data } = await response?.json();
 
-  return data
-}
+  return data;
+};
 
 export const getLayouts = async (
   site: string,
@@ -139,8 +167,8 @@ export const getLayouts = async (
   pageId: number
 ) => {
   if (!site || !siteId || !pageId)
-    throw new Error('Geen geldige site, siteId of pageId opgegeven')
-  const layouts = await fetchLayouts(site, siteId, pageId)
+    throw new Error("Geen geldige site, siteId of pageId opgegeven");
+  const layouts = await fetchLayouts(site, siteId, pageId);
 
-  return layouts
-}
+  return layouts;
+};
