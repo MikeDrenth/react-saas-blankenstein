d812b864e58fc2981fd420417cefb2e5ecbd29eb
Mike Drenth <mdrenth.1990@gmail.com>
2023-02-08 11:07:27 +0100

    Uitzetten niet gebruikte code

--- pages/api/tasks.ts
+++ pages/api/tasks.ts
@@ -1,20 +1,20 @@
-import { getLayouts } from "../../lib/getWebsiteInfo";
+// import { getLayouts } from "../../lib/getWebsiteInfo";
 
-import { NextApiRequest, NextApiResponse } from "next";
+// import { NextApiRequest, NextApiResponse } from "next";
 
-const handler = async (
-  req: NextApiRequest,
-  res: NextApiResponse
-): Promise<void> => {
-  const response = await getLayouts("blankensteinaanzee", 222, 63823);
-  const layouts = await response.json();
+// const handler = async (
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ): Promise<void> => {
+//   const response = await getLayouts("blankensteinaanzee", 222, 63823);
+//   const layouts = await response.json();
 
-  res.setHeader(
-    "Cache-Control",
-    "public, s-maxage=60, stale-while-revalidate=43200"
-  );
+//   res.setHeader(
+//     "Cache-Control",
+//     "public, s-maxage=60, stale-while-revalidate=43200"
+//   );
 
-  return res.status(200).json("hallo");
-};
+//   return res.status(200).json("hallo");
+// };
 
-export default handler;
+// export default handler;
